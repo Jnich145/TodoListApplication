9d47dc52d5f49ebe1bf701ce6767522184a7ea57
Justin <invicitusreshat@outlook.com>
2023-12-13 11:11:43 -0600

    Feature 8 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from todos.views import todo_list
+from todos.views import todo_list, todo_list_detail
 
 urlpatterns = [
-    path("", todo_list, name="todo_list"),
+    path("", todo_list, name="todo_list_list"),
+    path("<int:id>/", todo_list_detail, name="todo_list_detail"),
 ]
+
+# 2nd: In the todos urls.py file, register the view with the path "<int:id>/" and the name "todo_list_detail"
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from todos.models import TodoList
 # Create your views here.
 
@@ -8,3 +8,14 @@ def todo_list(request):
     "todo_list_list": list,
     }
     return render(request, "todos/todo_list.html", context)
+
+# 1st: Create a view that shows the details of a particular to-do list, including its tasks
+def todo_list_detail(request, id):
+    todo_list = get_object_or_404(TodoList, id=id)
+    tasks = todo_list.items.all()
+    context = {
+        "todo_list": todo_list,
+        "tasks": tasks,
+    }
+
+    return render(request, "todos/detail.html", context)
